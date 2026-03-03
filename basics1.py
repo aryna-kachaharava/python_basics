f1a6685f7446e7e40288632079f066bd61607027
Aryna Kachaharava <arishka21.03.2008@gmail.com>
2026-03-03 22:10:19 +0300

    added new part

--- basics1.py
+++ basics1.py
@@ -1,3 +1,11 @@
 name = input("Enter your name ")
 age = int(input("Enter your age "))
 print("Hello ", name, "! In 5 years you will be ", age + 5)
+print("Now about money")
+price=float(input("What is the price?"))
+paid=float(input("Which sum have you paid?"))
+if (paid>price):
+    print("Your return ", paid-price)
+elif (price>paid):
+    print("Not enough money")
+else: print("Thank u")
